be536481a01ec7d7be9a9ba473771420e16fe437
Md Muhibul Islam <112680631+mdmuhibul@users.noreply.github.com>
2022-10-13 19:30:49 -0400

    Add files via upload

---
+++ Exam 1 (Md muhibul Islam).R
@@ -0,0 +1,14 @@
+Md Muhibul Islam
+ECON B2000
+Exam 1
+
+View(Household_Pulse_data)
+summary(Household_Pulse_data)
+attach(Household_Pulse_data)
+Null hypothesis: The gender of the evaluator does not affect to telework days in the  past week.
+
+Question 02
+xtabs(~ HADCOVIDRV + RECVDVACC)
+
+xtabs(~ LONGCOVID + RECVDVACC)
+Null hypothesis: The gender of the evaluator does not affect population to tested covid positive.
